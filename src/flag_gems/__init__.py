59737deaa03de0bd62cbc8a83e50c4c91d202193
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-08-04 12:39:31 +0800

    Update __init__.py
    Signed-off-by: KK <102746679+Yukun-Cui@users.noreply.github.com>

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -87,8 +87,8 @@ _FULL_CONFIG = (
     ("_chunk_cat", _chunk_cat),
     ("_conj", _conj),
     ("_conv_depthwise2d", _conv_depthwise2d),
-    ("_dyn_quant_pack_4bit_weight", _dyn_quant_pack_4bit_weight),
     ("_convert_weight_to_int4pack", _convert_weight_to_int4pack),
+    ("_dyn_quant_pack_4bit_weight", _dyn_quant_pack_4bit_weight),
     ("_efficient_attention_backward", efficient_attention_backward),
     ("_embedding_bag_dense_backward", _embedding_bag_dense_backward),
     (
