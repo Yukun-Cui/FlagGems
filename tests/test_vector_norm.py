d461107ddd9040712d6e60e58a4afcae442dca6e
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-08-04 15:21:52 +0800

    Update test_vector_norm.py
    Signed-off-by: KK <102746679+Yukun-Cui@users.noreply.github.com>

--- tests/test_vector_norm.py
+++ tests/test_vector_norm.py
@@ -113,6 +113,3 @@ def test_accuracy_linalg_vector_norm_flatten(shape, dtype):
         res_out = torch.linalg.vector_norm(inp)
 
     utils.gems_assert_close(res_out, ref_out, dtype)
-    utils.gems_assert_close(
-        res_out, ref_out, dtype, reduce_dim=_get_reduce_dim(shape, dim)
-    )
