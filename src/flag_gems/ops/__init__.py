778c0493522fff6b3d293d2151464b318a371bef
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-08-16 11:24:47 +0000

    [KernelGen][Nvidia] Add _coalesced_ operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -29,6 +29,7 @@ from flag_gems.ops._amp_foreach_non_finite_check_and_unscale_ import (
 )
 from flag_gems.ops._batch_norm_no_update import _batch_norm_no_update
 from flag_gems.ops._chunk_cat import chunk_cat as _chunk_cat
+from flag_gems.ops._coalesced_ import _coalesced_
 from flag_gems.ops._conj import _conj
 from flag_gems.ops._convert_weight_to_int4pack import _convert_weight_to_int4pack
 from flag_gems.ops._dyn_quant_pack_4bit_weight import _dyn_quant_pack_4bit_weight
@@ -815,6 +816,7 @@ __all__ = [
     "_amp_foreach_non_finite_check_and_unscale_",
     "_assert_async",
     "_batch_norm_no_update",
+    "_coalesced_",
     "_functional_assert_async",
     "_cdist_backward",
     "_cdist_forward",
