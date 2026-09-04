069129a68966d4fbf79628ad9a205961bc2dbd30
Yukun-Cui <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-04 09:12:13 +0000

    [CI] Fix _stack operators.yaml: add stages alpha 5.4

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -305,8 +305,8 @@ _FULL_CONFIG = (
     ("_sparse_semi_structured_addmm", _sparse_semi_structured_addmm),
     ("_sparse_semi_structured_linear", _sparse_semi_structured_linear),
     ("_sparse_semi_structured_mm", _sparse_semi_structured_mm),
-    ("_standard_gamma_grad", standard_gamma_grad),
     ("_stack", _stack),
+    ("_standard_gamma_grad", standard_gamma_grad),
     (
         "_thnn_differentiable_gru_cell_backward",
         _thnn_differentiable_gru_cell_backward,
