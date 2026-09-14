5e42cf40d8c4651c2c1a249b5e3efff0329a6f3b
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-14 10:08:37 +0000

    Add test and benchmark changes
    - tests/test_log_softmax.py: Remove use_gems(), call flag_gems.log_softmax* directly, use torch._log_softmax for reference
    - benchmark/test_log_softmax.py: Replace torch.nn.functional.log_softmax with torch._log_softmax (with dim/half_to_float args)

--- benchmark/test_log_softmax.py
+++ benchmark/test_log_softmax.py
@@ -37,7 +37,7 @@ def test_log_softmax():
     bench = base.GenericBenchmark2DOnly(
         op_name="log_softmax",
         input_fn=utils.unary_input_fn,
-        torch_op=torch.nn.functional.log_softmax,
+        torch_op=torch._log_softmax,
         dtypes=consts.FLOAT_DTYPES,
     )
     bench.run()
@@ -64,14 +64,14 @@ def test_log_softmax_out():
 def test_log_softmax_backward_data():
     def log_softmax_backward_data_input_fn(shape, dtype, device):
         inp = torch.randn(shape, dtype=dtype, device=device)
-        output = torch.nn.functional.log_softmax(inp, dim=-1)
+        output = torch._log_softmax(inp, dim=-1, half_to_float=False)
         grad_output = torch.randn_like(output)
         yield grad_output, output, -1, dtype
 
     bench = base.GenericBenchmark2DOnly(
         op_name="log_softmax_backward_data",
         input_fn=log_softmax_backward_data_input_fn,
-        torch_op=torch.ops.aten._log_softmax_backward_data,
+        torch_op=torch._log_softmax_backward_data,
         dtypes=consts.FLOAT_DTYPES,
     )
     bench.run()
@@ -79,7 +79,7 @@ def test_log_softmax_backward_data():
 
 def log_softmax_backward_data_out_input_fn(shape, dtype, device):
     inp = torch.randn(shape, dtype=dtype, device=device)
-    log_sm = torch.nn.functional.log_softmax(inp, dim=-1)
+    log_sm = torch._log_softmax(inp, dim=-1, half_to_float=False)
     grad_output = torch.randn_like(log_sm)
     out = torch.empty_like(grad_output)
     yield grad_output, log_sm, -1, dtype, {"out": out}
