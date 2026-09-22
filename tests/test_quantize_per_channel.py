8aabd42fe26dcf2b22de9119693634834ccc2a53
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-22 11:49:47 +0800

    Update test_quantize_per_channel.py
    Signed-off-by: KK <102746679+Yukun-Cui@users.noreply.github.com>

--- tests/test_quantize_per_channel.py
+++ tests/test_quantize_per_channel.py
@@ -12,11 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import subprocess
-import sys
-import textwrap
-
 import pytest
 import torch
 
